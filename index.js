2a8e6dd01550fd74aacd5b4f04a1431df758ecf4
OscarGE11 <geoscar1100@gmail.com>
2024-07-15 21:23:17 +0200

    added movement routes and controllers

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 import express from 'express';
 import connectDB from './database.js';
 import config from './src/config/config.js';
+import movementRouter from './src/routes/movement.routes.js';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = config.port;
 
 // Middleware para parsear JSON
 app.use(express.json());
-
-// Conectar a la base de datos
-connectDB();
-
+// Middleware para rastrear las peticiones HTTP
+app.use(morgan('dev'));
 // Middleware para manejar errores
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+
+// Middleware para gestionar rutas
+app.use('/api/movements', movementRouter);
+// Conectar a la base de datos
+connectDB();
 
 // Iniciar el servidor
 app.listen(PORT, () => {
--- src/models/Movement.js
+++ src/models/Movement.js
@@ -31,20 +31,3 @@ const movementSchema = new mongoose.Schema(
 );
 
 export const MovementModel = mongoose.model('Movement', movementSchema);
-
-export const createMovement = async () => {
-  const movement = new MovementModel({
-    title: 'cena Vips',
-    description: 'cena vips en parque sur el martes',
-    amount: 1000,
-    typeOfMovement: 'expense',
-    category: '66953094a13623ccfcd0b6eb',
-  });
-  const result = await movement.save();
-  console.log(result);
-};
-
-export const showMovements = async () => {
-  const movements = await MovementModel.find().populate('category', 'name');
-  console.log(movements);
-};
