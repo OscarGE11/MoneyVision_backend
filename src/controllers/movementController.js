0614660944075bc3ea5b37a5ba87d6c567c66b5f
OscarGE11 <geoscar1100@gmail.com>
2024-07-15 22:39:28 +0200

    added CRUD operations movementController

--- src/controllers/movementController.js
+++ src/controllers/movementController.js
@@ -1,6 +1,6 @@
 import { MovementModel } from '../models/Movement.js';
 
-// Functior for creating a movement
+// Function for creating a movement
 export const createMovement = async (req, res) => {
   const { title, description, amount, typeOfMovement, category } = req.body;
 
@@ -27,5 +27,53 @@ export const getAllMovements = async (req, res) => {
     res.status(200).json(movements);
   } catch (err) {
     res.status(500).json({ message: 'Error getting the movements', err });
+  }
+};
+
+// Function for getting one movement
+
+export const getMovementByID = async (req, res) => {
+  try {
+    const movement = await MovementModel.findById(req.params.id).populate(
+      'category',
+      'name'
+    );
+
+    if (!movement) {
+      return res.status(404).json({
+        message: 'User not found',
+      });
+    }
+    res.status(200).json(movement);
+  } catch (err) {
+    res.status(500).json({ message: 'Error getting the movement', err });
+  }
+};
+
+export const updateMovement = async (req, res) => {
+  try {
+    const movementUpdate = await MovementModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(202).json(movementUpdate);
+
+    if (!movementUpdate) {
+      res.status(404).json({ message: 'Movement not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Error updating the movement' });
+  }
+};
+
+export const deleteMovement = async (req, res) => {
+  try {
+    await MovementModel.findByIdAndDelete(req.params.id);
+    res.status(204).json({ message: 'Movement deleted' });
+  } catch (err) {
+    res.status(500).json({ message: 'Error deleting the movement' });
   }
 };
--- src/routes/movement.routes.js
+++ src/routes/movement.routes.js
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import {
   createMovement,
+  deleteMovement,
   getAllMovements,
+  getMovementByID,
+  updateMovement,
 } from '../controllers/movementController.js';
 
 const movementRouter = Router();
 
 movementRouter.post('/', createMovement);
 movementRouter.get('/', getAllMovements);
+movementRouter.get('/:id', getMovementByID);
+movementRouter.put('/:id', updateMovement);
+movementRouter.delete('/:id', deleteMovement);
 
 export default movementRouter;
