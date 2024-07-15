8b511b8f6cd613fdffe5f0ff372718695db6dca2
alvaro <alvaro.apariciofdz@gmail.com>
2024-07-15 21:30:45 +0200

    fixed import

--- src/routes/movement.routes.js
+++ src/routes/movement.routes.js
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import { createMovement } from '../controllers/movementController.js';
-import { showMovements } from '../models/Movement.js';
 
 const movementRouter = Router();
 
 movementRouter.post('/', createMovement);
-movementRouter.get('/', showMovements);
 
 export default movementRouter;
